49aed8fd2eb31f3b006fa7f0e452a32242be1613
Choco <chocothedwarf@gmail.com>
2024-06-14 11:08:36 -0400

    finished adding prefabs to resource name lists

--- Scripts/Game/WR_LootSpawnContextPresets.c
+++ Scripts/Game/WR_LootSpawnContextPresets.c
@@ -5,18 +5,18 @@ class WR_LootSpawnContextPresets
     {
         return new WR_LootSpawnContext
         (
-            rifleWeight: 500.0,
+            rifleWeight: 5.0,
             machineGunWeight: 1.0,
             sniperWeight: 1.0,
-            handgunWeight: 1.0,
-            launcherWeight: 500.0,
+            handgunWeight: 5.0,
+            launcherWeight: 1.0,
             ordnanceWeight: 1.0,
-            attachmentWeight: 1.0,
-            utilItemWeight: 1.0,
-            mediItemWeight: 1.0,
-            backpackWeight: 1.0,
-            vestWeight: 1.0,
-            headgearWeight: 1.0
+            attachmentWeight: 10.0,
+            utilItemWeight: 5.0,
+            mediItemWeight: 10.0,
+            backpackWeight: 5.0,
+            vestWeight: 5.0,
+            headgearWeight: 5.0
         );
     }
 
@@ -24,18 +24,18 @@ class WR_LootSpawnContextPresets
     {
         return new WR_LootSpawnContext
         (
-            rifleWeight: 1.0,
-            machineGunWeight: 500.0,
+            rifleWeight: 10.0,
+            machineGunWeight: 1.0,
             sniperWeight: 1.0,
-            handgunWeight: 1.0,
+            handgunWeight: 10.0,
             launcherWeight: 1.0,
             ordnanceWeight: 1.0,
             attachmentWeight: 1.0,
             utilItemWeight: 1.0,
-            mediItemWeight: 1.0,
-            backpackWeight: 500.0,
-            vestWeight: 1.0,
-            headgearWeight: 1.0
+            mediItemWeight: 5.0,
+            backpackWeight: 5.0,
+            vestWeight: 5.0,
+            headgearWeight: 5.0
         );
     }
 }
