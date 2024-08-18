be81e1ef1def9b27e0e6e0b412b5c473511af43d
Paul Andrews <paul2kandrews@gmail.com>
2024-08-18 13:33:56 -0700

    Fix vehicle boarding logic to work with indie groups

--- Scripts/Game/UserActions/SCR_GetInUserActionModded.c
+++ Scripts/Game/UserActions/SCR_GetInUserActionModded.c
@@ -5,40 +5,6 @@
 modded class SCR_GetInUserAction : SCR_CompartmentUserAction
 {
 	//------------------------------------------------------------------------------------------------
-	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
-	{
-		if (!pOwnerEntity || !pUserEntity)
-			return;
-		
-		ChimeraCharacter character = ChimeraCharacter.Cast(pUserEntity);
-		if (!character)
-			return;
-		
-		BaseCompartmentSlot targetCompartment = GetCompartmentSlot();
-		if (!targetCompartment)
-			return;
-		
-		CompartmentAccessComponent compartmentAccess = character.GetCompartmentAccessComponent();
-		if (!compartmentAccess)
-			return;
-		Vehicle vehicle = Vehicle.Cast(SCR_EntityHelper.GetMainParent(pOwnerEntity, true));
-		FactionKey origFaction; 
-		if (!vehicle)
-			return;
-			
-		SCR_VehicleFactionAffiliationComponent affiliation = vehicle.GetFactionAffiliation();
-		origFaction = affiliation.GetAffiliatedFactionKey();
-		affiliation.SetAffiliatedFactionByKey("friendly");
-		
-		if (!compartmentAccess.GetInVehicle(pOwnerEntity, targetCompartment, false, GetRelevantDoorIndex(pUserEntity), ECloseDoorAfterActions.RETURN_TO_PREVIOUS_STATE, false))
-			return;
-		
-		affiliation.SetAffiliatedFactionByKey(origFaction);
-		
-		super.PerformAction(pOwnerEntity, pUserEntity);
-	}
-	
-	//------------------------------------------------------------------------------------------------
 	// Override to allow players to enter vehicles occupied by enemies
 	override bool CanBePerformedScript(IEntity user)
 	{
@@ -69,18 +35,64 @@ modded class SCR_GetInUserAction : SCR_CompartmentUserAction
 		}
 		
 		IEntity owner = compartment.GetOwner();
-		
-//		Vehicle vehicle = Vehicle.Cast(SCR_EntityHelper.GetMainParent(owner, true));
-//		if (vehicle)
-//		{
-//			Faction characterFaction = character.GetFaction();
-//			Faction vehicleFaction = vehicle.GetFaction();
-//			if (characterFaction && vehicleFaction && characterFaction.IsFactionEnemy(vehicleFaction))
-//			{
-//				SetCannotPerformReason("#AR-UserAction_SeatHostile");
-//				return false;
-//			}
-//		}
+		Vehicle vehicle = Vehicle.Cast(SCR_EntityHelper.GetMainParent(owner, true));
+		if (vehicle)
+		{
+			SCR_BaseCompartmentManagerComponent baseCompMan = SCR_BaseCompartmentManagerComponent.Cast(vehicle.FindComponent(SCR_BaseCompartmentManagerComponent));
+			if (!baseCompMan)
+				return false;
+			
+			if (baseCompMan.GetOccupantCount() > 0)
+			{
+				Faction characterFaction = character.GetFaction();
+			
+				// Factions not friendly to themselves (Independent, in this case)
+				if (characterFaction.IsFactionEnemy(characterFaction))
+				{
+					array<IEntity> occupants = {};
+					baseCompMan.GetOccupants(occupants);
+					
+					PlayerManager playerManager = GetGame().GetPlayerManager();
+					SCR_GroupsManagerComponent groupManager = SCR_GroupsManagerComponent.GetInstance();
+					
+					int characterPlayerId = playerManager.GetPlayerIdFromControlledEntity(character);
+					SCR_AIGroup characterGroup = groupManager.GetPlayerGroup(characterPlayerId);
+					
+					// Allow action to be performed if any occupant of the vehicle is in the same squad as the character
+					// Begin by assuming that the vehicle holds no squad members of the player
+					bool noOccupantsAreMemberOfSquad = true;
+					foreach (IEntity occupant : occupants)
+					{	
+						int occupantPlayerId = playerManager.GetPlayerIdFromControlledEntity(occupant);
+						SCR_AIGroup occupantGroup = groupManager.GetPlayerGroup(occupantPlayerId);
+						
+						// If this occupant is a member of the player's squad, the original assumption is wrong and we can stop
+						if (characterGroup == occupantGroup && characterGroup != null && occupantGroup != null)
+						{
+							noOccupantsAreMemberOfSquad = false;
+							break;
+						}
+					}
+					
+					// Vehicle is hostile if no squad members were found inside.
+					if (noOccupantsAreMemberOfSquad)
+					{
+						SetCannotPerformReason("#AR-UserAction_SeatHostile");
+						return false;
+					}
+				}
+				// Any other faction
+				else
+				{
+					Faction vehicleFaction = vehicle.GetFaction();
+					if (characterFaction && vehicleFaction && characterFaction.IsFactionEnemy(vehicleFaction))
+					{
+						SetCannotPerformReason("#AR-UserAction_SeatHostile");
+						return false;
+					}
+				}	
+			}
+		}
 		
 		if (compartment.GetOccupant())
 		{
--- Scripts/Game/UserActions/SCR_ToggleHandbrakeActionModded.c
+++ Scripts/Game/UserActions/SCR_ToggleHandbrakeActionModded.c
@@ -14,13 +14,63 @@ modded class SCR_ToggleHandbrakeAction : SCR_ScriptedUserAction
 		if (!character)
 			return false;
 
-//		Faction characterFaction = character.GetFaction();
-//		Faction vehicleFaction = vehicleEntity.GetFaction();
-//		if (characterFaction && vehicleFaction && characterFaction.IsFactionEnemy(vehicleFaction))
-//		{
-//			SetCannotPerformReason(OCCUPIED_BY_ENEMY);
-//			return false;
-//		}
+		if (vehicleEntity)
+		{
+			SCR_BaseCompartmentManagerComponent baseCompMan = SCR_BaseCompartmentManagerComponent.Cast(vehicleEntity.FindComponent(SCR_BaseCompartmentManagerComponent));
+			if (!baseCompMan)
+				return false;
+			
+			if (baseCompMan.GetOccupantCount() > 0)
+			{
+				Faction characterFaction = character.GetFaction();
+			
+				// Factions not friendly to themselves (Independent, in this case)
+				if (characterFaction.IsFactionEnemy(characterFaction))
+				{
+					array<IEntity> occupants = {};
+					baseCompMan.GetOccupants(occupants);
+					
+					PlayerManager playerManager = GetGame().GetPlayerManager();
+					SCR_GroupsManagerComponent groupManager = SCR_GroupsManagerComponent.GetInstance();
+					
+					int characterPlayerId = playerManager.GetPlayerIdFromControlledEntity(character);
+					SCR_AIGroup characterGroup = groupManager.GetPlayerGroup(characterPlayerId);
+					
+					// Allow action to be performed if any occupant of the vehicle is in the same squad as the character
+					// Begin by assuming that the vehicle holds no squad members of the player
+					bool noOccupantsAreMemberOfSquad = true;
+					foreach (IEntity occupant : occupants)
+					{	
+						int occupantPlayerId = playerManager.GetPlayerIdFromControlledEntity(occupant);
+						SCR_AIGroup occupantGroup = groupManager.GetPlayerGroup(occupantPlayerId);
+						
+						// If this occupant is a member of the player's squad, the original assumption is wrong and we can stop
+						if (characterGroup == occupantGroup && characterGroup != null && occupantGroup != null)
+						{
+							noOccupantsAreMemberOfSquad = false;
+							break;
+						}
+					}
+					
+					// Vehicle is hostile if no squad members were found inside.
+					if (noOccupantsAreMemberOfSquad)
+					{
+						SetCannotPerformReason("#AR-UserAction_SeatHostile");
+						return false;
+					}
+				}
+				// Any other faction
+				else
+				{
+					Faction vehicleFaction = vehicleEntity.GetFaction();
+					if (characterFaction && vehicleFaction && characterFaction.IsFactionEnemy(vehicleFaction))
+					{
+						SetCannotPerformReason("#AR-UserAction_SeatHostile");
+						return false;
+					}
+				}	
+			}
+		}
 
 		if (!SCR_InteractionHandlerComponent.CanBeShownInVehicle(
 			character,
