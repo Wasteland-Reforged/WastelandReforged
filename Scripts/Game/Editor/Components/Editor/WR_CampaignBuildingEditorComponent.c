483456b477ff0ff0f7ba0ee1adda8b6242f0f69c
Choco <chocothedwarf@gmail.com>
2024-11-05 19:35:40 -0500

    Fixed vehicle building factions

---
+++ Scripts/Game/Editor/Components/Editor/WR_CampaignBuildingEditorComponent.c
@@ -0,0 +1,12 @@
+modded class SCR_CampaignBuildingEditorComponentClass : SCR_BaseEditorComponentClass
+{
+}
+
+modded class SCR_CampaignBuildingEditorComponent : SCR_BaseEditorComponent
+{
+	SCR_FactionAffiliationComponent GetProviderFactionComponent()
+	{
+
+		return null;
+	}
+}
