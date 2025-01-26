8d510cce332517726c6415ba0808d65032e15c28
Choco <chocothedwarf@gmail.com>
2025-01-26 16:05:46 -0500

    fixed prop spawns for vehicle wreck and sniper squad

--- Scripts/Game/Loot/WR_LootSpawner.c
+++ Scripts/Game/Loot/WR_LootSpawner.c
@@ -56,8 +56,10 @@ class WR_LootSpawner : GenericEntity
 		
 			//Spawn the item
 			IEntity newEnt = GetGame().SpawnEntityPrefab(m_Resource, GetGame().GetWorld(), params);
-			if (!newEnt)
+			if (!newEnt) {
+				logger.LogError("Failed to spawn item: " + item);
 				return false;
+			}
 
 			//All Items: Rotated randomly horizontally
 			//Guns/Mags: Placed sideways
@@ -74,7 +76,7 @@ class WR_LootSpawner : GenericEntity
 				newEnt.SetYawPitchRoll(GetYawPitchRoll() + m_vItemRotation);
 			}
 			SCR_EntityHelper.SnapToGround(newEnt);
-			newEnt.SetOrigin(newEnt.GetOrigin() + {0.0, 0.015, 0.0});
+			//newEnt.SetOrigin(newEnt.GetOrigin() + {0.0, 0.015, 0.0});
 		
 			AddChild(newEnt, -1, EAddChildFlags.NONE);
 			firstItem = false;
