95e7c3537fa28e78df22f406d94d0593c1a1414f
Paul Andrews <paul2kandrews@gmail.com>
2025-04-21 22:22:37 -0700

    Remove rank from player map markers Switch arland mission config back

---
+++ src/Core/Scripts/Game/FPM/Map/Markers/Config/FPM_MapMarkerEntryPlayerModded.c
@@ -0,0 +1,8 @@
+modded class FPM_MapMarkerEntryPlayer
+{
+	override string GetPlayerNameWithRank(int playerId, IEntity player)
+	{
+		string name = GetGame().GetPlayerManager().GetPlayerName(playerId);
+		return name;
+	}
+}
