9450531e4bff2de232b4c9ba78950f98eca021de
mohred91 <battlefieldbenelux@gmail.com>
2026-07-04 15:36:57 +0200

    Fix IsEnemyFaction signature for AR 1.7

--- src/Core/Scripts/Game/Building/Modded/SCR_CampaignBuildingProviderComponent.c
+++ src/Core/Scripts/Game/Building/Modded/SCR_CampaignBuildingProviderComponent.c
@@ -4,12 +4,7 @@ modded class SCR_CampaignBuildingProviderComponentClass : SCR_MilitaryBaseLogicC
 
 modded class SCR_CampaignBuildingProviderComponent : SCR_MilitaryBaseLogicComponent
 {
-	override bool IsCharacterFactionSame(notnull IEntity character)
-	{
-		return true;
-	}
-	
-	override bool IsEnemyFaction(notnull ChimeraCharacter char)
+	override bool IsEnemyFaction(notnull SCR_ChimeraCharacter char)
 	{
 		FactionAffiliationComponent factionComponent = FactionAffiliationComponent.Cast(char.FindComponent(FactionAffiliationComponent));
 		if (!factionComponent)
