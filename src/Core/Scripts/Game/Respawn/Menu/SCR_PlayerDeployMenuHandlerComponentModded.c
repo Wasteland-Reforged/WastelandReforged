8bccfad7264a5c8633401abd0b82e789e501d200
mohred91 <battlefieldbenelux@gmail.com>
2026-07-19 13:44:17 +0200

    Update SCR_PlayerDeployMenuHandlerComponentModded.c
    Restore [WASTELAND] tag in log message

--- src/Core/Scripts/Game/Respawn/Menu/SCR_PlayerDeployMenuHandlerComponentModded.c
+++ src/Core/Scripts/Game/Respawn/Menu/SCR_PlayerDeployMenuHandlerComponentModded.c
@@ -69,7 +69,7 @@ modded class SCR_PlayerDeployMenuHandlerComponent
 		SCR_SpawnPoint spawnPoint = SCR_SpawnPoint.Cast(GetGame().GetWorld().FindEntityByName(spawnPointName));
 		if (!spawnPoint)
 		{
-			Print(string.Format("Spawn point '%1' should be present in the world if a spawn lobby is also present!", spawnPointName), LogLevel.ERROR);
+			Print(string.Format("[WASTELAND] SCR_PlayerDeployMenuHandlerComponent: Spawn point '%1' should be present in the world if a spawn lobby is also present!", spawnPointName), LogLevel.ERROR);
 			return false;
 		}
 		
